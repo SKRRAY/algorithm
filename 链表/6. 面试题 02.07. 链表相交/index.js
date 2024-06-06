4062594c75f8bce1c617946595045ddc153645f0
liuzilei3 <liuzilei3@jd.com>
2024-06-06 17:12:14 +0800

    feat: 链表相交

---
+++ 链表/6. 面试题 02.07. 链表相交/index.js
@@ -0,0 +1,42 @@
+/*
+ * @Autor: ERP
+ * @Email: 邮箱
+ * @Description: 面试题 02.07. 链表相交
+ * @CreateDate: Do not edit
+ * @LastEditors: liuzilei3
+ */
+/* 
+题目链接：https://leetcode.cn/problems/intersection-of-two-linked-lists-lcci/description/
+题解： https://leetcode.cn/problems/intersection-of-two-linked-lists-lcci/solutions/1395092/lian-biao-xiang-jiao-by-leetcode-solutio-2kne/
+
+给你两个单链表的头节点 headA 和 headB ，请你找出并返回两个单链表相交的起始节点。如果两个链表没有交点，返回 null 。
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function(headA, headB) {
+    var getIntersectionNode = function(headA, headB) {
+        if (headA === null || headB === null) {
+            return null;
+        }
+        let pA = headA, pB = headB;
+        while (pA !== pB) {
+            pA = pA === null ? headB : pA.next;
+            pB = pB === null ? headA : pB.next;
+        }
+        return pA;
+    };
+};
+
