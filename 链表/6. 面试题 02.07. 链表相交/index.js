e60b23f49700dd905de292a2a4cd0f337c073289
liuzilei3 <liuzilei3@jd.com>
2024-07-02 16:47:40 +0800

    feat: 环形链表II

--- 链表/6. 面试题 02.07. 链表相交/index.js
+++ 链表/6. 面试题 02.07. 链表相交/index.js
@@ -27,16 +27,14 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
-    var getIntersectionNode = function(headA, headB) {
-        if (headA === null || headB === null) {
-            return null;
-        }
-        let pA = headA, pB = headB;
-        while (pA !== pB) {
-            pA = pA === null ? headB : pA.next;
-            pB = pB === null ? headA : pB.next;
-        }
-        return pA;
-    };
+    if (headA === null || headB === null) {
+        return null;
+    }
+    let pA = headA, pB = headB;
+    while (pA !== pB) {
+        pA = pA === null ? headB : pA.next;
+        pB = pB === null ? headA : pB.next;
+    }
+    return pA;
 };
 
